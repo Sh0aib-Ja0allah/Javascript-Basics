9590640381fb9efd308d9072d7f74801eaf9828e
Shoaib Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2023-04-17 03:35:21 +0300

    Update ProplemSolving.js

--- ProplemSolving.js
+++ ProplemSolving.js
@@ -2,12 +2,13 @@
 function evenOrOdd(number) {
   return number % 2 == 0? "Even" : "Odd"
 }
-
+console.log("\n-------------------------------")
+console.log("# the check of even or odd number is :")
 console.log(evenOrOdd(7))
 console.log(evenOrOdd(4))
+console.log("-------------------------------")
 
-
-//Return the number (count) of vowels in the given string. --  We will consider a, e, i, o, u as vowels.
+//Return the number (count) of vowels in the given string. --  We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 function getCount(str) {
     const vowelCharacters = ['a', 'i', 'e', 'u', 'o'];
@@ -19,3 +20,8 @@ function getCount(str) {
     }
     return count;
 }
+console.log("\n-------------------------------")
+console.log("# Number of vowel characters is :")
+console.log(getCount("Jadallah"))
+console.log(getCount("abracadabra"))
+console.log("-------------------------------")
