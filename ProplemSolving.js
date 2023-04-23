89cf947557fadb83b57bf808d58bf78188c4e5d6
Shoaib Jadallah <110256562+Sh0aib-Ja0allah@users.noreply.github.com>
2023-04-24 01:14:08 +0300

    Update ProplemSolving.js

--- ProplemSolving.js
+++ ProplemSolving.js
@@ -77,7 +77,33 @@ function disemvowel(str) {
 
 console.log("\n-----------------------------")
 console.log("# Function to return a string without it's vowel letters :")
-console.log(disemvowel("This website is for losers LOL!"))      //  Ths wbst s fr lsrs LL!
+console.log( " This website is for losers LOL!   => " , disemvowel("This website is for losers LOL!"))      //  Ths wbst s fr lsrs LL!
+console.log("-----------------------------")
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
+Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+function solution(number){
+  let sum = 0
+  for(let i = 0; i < number; i++){
+    if(i%3 == 0 || i%5 == 0){
+      sum += i
+    }
+  }
+  return sum
+}
+
+console.log("\n-----------------------------")
+console.log("# Function to return the sum of the numbers \n that can divide ( 3 or 5 ) :")
+console.log( " 10    => " , solution(10))      //   23
+console.log( " 20    => " , solution(20))      //   78
+console.log( " 200   => " , solution(200))     //   9168
 console.log("-----------------------------")
 
 console.log("\nEnd")
